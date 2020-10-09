aedda5831625ce142d7e2030dc33a37158172fa8
RebekahJulicher <70235428+RebekahJulicher@users.noreply.github.com>
2020-10-08 20:32:17 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/imagequiz/precache-manifest.c5be4b5f7c1299ae4482327a5654e150.js"
+  "/imagequiz/precache-manifest.6c1f052c0734b628362dea2ccf83cca1.js"
 );
 
 self.addEventListener('message', (event) => {
