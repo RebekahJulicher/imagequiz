05e40a9b6a67f21da9ec45ee444cf031045d1d88
RebekahJulicher <70235428+RebekahJulicher@users.noreply.github.com>
2020-10-08 20:32:29 -0700

    Done

--- imagequiz/src/components/Home.js
+++ imagequiz/src/components/Home.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css';
+import cherry from "../../src/images/cherryblossom.png";
+import daffodil from "../../src/images/daffodil.png";
+import daisy from "../../src/images/daisy.jpg";
+import lily from "../../src/images/daisy.jpg";
+import rose from "../../src/images/rose.png";
+import sunflower from "../../src/images/sunflower.png";
+import tulip from "../../src/images/tulip.png";
+import waterlily from "../../src/images/waterlily.png";
 
 class Home extends React.Component {
     constructor(props) {
@@ -27,7 +35,59 @@ class Home extends React.Component {
                     {username.length > 0 ? username
                     : <Link to='/login'>Login</Link>}
                 </div>
-                <div>Homepage Starter Text</div>
+                <div className="headerDiv">
+                    <h1>Big Ol' Homepage</h1>
+                </div>
+                <div className="pictureDiv">
+                    <figure className="figure">
+                        <img src={cherry} />
+                        <figcaption>
+                            Cherry Blossom
+                        </figcaption>
+                    </figure>
+                    <figure className="figure">
+                        <img src={daffodil} />
+                        <figcaption>
+                            Daffodil
+                        </figcaption>
+                    </figure>
+                    <figure className="figure">
+                        <img src={daisy} />
+                        <figcaption>
+                            Daisy
+                        </figcaption>
+                    </figure>
+                    <figure className="figure">
+                        <img src={lily} />
+                        <figcaption>
+                            Lily
+                        </figcaption>
+                    </figure>
+                    <figure className="figure">
+                        <img src={rose} />
+                        <figcaption>
+                            Rose
+                        </figcaption>
+                    </figure>
+                    <figure className="figure">
+                        <img src={sunflower} />
+                        <figcaption>
+                            Sunflower
+                        </figcaption>
+                    </figure>
+                    <figure className="figure">
+                        <img src={tulip} />
+                        <figcaption>
+                            Tulip
+                        </figcaption>
+                    </figure>
+                    <figure className="figure">
+                        <img src={waterlily} />
+                        <figcaption>
+                            Waterlily
+                        </figcaption>
+                    </figure>
+                </div>
             </div>
         )
     }
